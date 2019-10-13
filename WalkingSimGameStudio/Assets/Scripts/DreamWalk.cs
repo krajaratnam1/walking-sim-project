0b71a69a6d8090edb26732e40acd8827fd753210
Krishan Rajaratnam <kr2481@nyu.edu>
2019-10-13 02:04:43 -0400

    VietnamSubs
    Vietnamese tonal marker substitution prototype quickly coded up for demo purposes to try and simulate a "vietnamese accent" in text form. Variable rate and mutation probability can make this accent fluctuate in how obvious it is...

---
+++ WalkingSimGameStudio/Assets/Scripts/DreamWalk.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DreamWalk : MonoBehaviour
+{
+    public GameObject dreamer, dreamCam;
+    public RawImage dreamScreen;
+    public float MS = 0.1f;
+
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -90F;
+    public float maximumY = 90F;
+    float rotationY = 0F;
+
+    public float fadeTime = 5.0f, timeUntilFade = 3f, timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void fading()
+    {
+        timer += Time.deltaTime;
+        if(timer >= timeUntilFade)
+        {
+            float time2 = timer - timeUntilFade;
+            if(time2 >= fadeTime)
+            {
+                dreamScreen.gameObject.SetActive(false);
+                dreamer.SetActive(false);
+                return;
+            }
+            dreamScreen.color = new Color(dreamScreen.color.r, dreamScreen.color.g, dreamScreen.color.b, 1 - (time2 / fadeTime));
+        }
+    }
+
+    void mouseCamera(GameObject cam, GameObject player = null)
+    {
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = cam.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            if (player == null)
+            {
+                cam.transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+            }
+            else
+            {
+                cam.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+                player.transform.localEulerAngles = new Vector3(0, rotationX, 0);
+            }
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            if (player == null)
+            {
+                cam.transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            }
+            else
+            {
+                player.transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            }
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            if (player == null)
+            {
+                cam.transform.localEulerAngles = new Vector3(-rotationY, cam.transform.localEulerAngles.y, 0);
+            }
+            else
+            {
+                cam.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+            }
+        }
+
+
+        if (minimumX > -360 && maximumX < 360)
+        {
+            print(minimumX);
+            if (cam.transform.localEulerAngles.y <= maximumX && cam.transform.localEulerAngles.y >= minimumX)
+            {
+                // ok
+            }
+            else if (cam.transform.localEulerAngles.y >= 360 + minimumX)
+            {
+                // ok
+            }
+            else
+            {
+                float high = cam.transform.localEulerAngles.y - maximumX;
+                float low = cam.transform.localEulerAngles.y - (360 + minimumX);
+                if (Mathf.Abs(high) < Mathf.Abs(low) && cam.transform.localEulerAngles.y >= minimumX)
+                {
+                    cam.transform.localEulerAngles = new Vector3(cam.transform.localEulerAngles.x, maximumX, cam.transform.localEulerAngles.z);
+                }
+                else
+                {
+                    cam.transform.localEulerAngles = new Vector3(cam.transform.localEulerAngles.x, minimumX, cam.transform.localEulerAngles.z);
+                }
+            }
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fading();
+        if (Input.GetKey(KeyCode.LeftArrow) ^ Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 moveDir = new Vector3(Input.GetKey(KeyCode.LeftArrow) ? 1 : -1, 0, 0);
+            Quaternion rotation = Quaternion.Euler(0, dreamCam.transform.eulerAngles.y + 180, 0);
+            moveDir = rotation * moveDir; // rotating so left is appropriate
+            moveDir *= MS; // movement speed
+            dreamer.GetComponent<CharacterController>().Move(moveDir);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) ^ Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 moveDir = new Vector3(Input.GetKey(KeyCode.UpArrow) ? 1 : -1, 0, 0);
+            Quaternion rotation = Quaternion.Euler(0, dreamCam.transform.eulerAngles.y - 90, 0);
+            moveDir = rotation * moveDir; // rotating so forward is appropriate
+            moveDir *= MS; // movement speed
+            dreamer.GetComponent<CharacterController>().Move(moveDir);
+        }
+        mouseCamera(dreamCam);
+    }
+}
---
+++ WalkingSimGameStudio/Assets/Scripts/VietnamSubs.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VietnamSubs : MonoBehaviour
+{
+    public Text text;
+    public string origString;
+    public float subProb = 0.5f;
+    public float timeToSub = 0.1f, timer = 0;
+
+    string subChar(string c)
+    {
+        if (c == "A")
+        {
+            string[] subArray = { "Ă", "Â", "Ằ", "Ầ", "Ẳ", "Ẩ", "Ẵ", "Ẫ", "Ắ", "Ấ", "Ặ", "Ậ" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "a")
+        {
+            string[] subArray = { "ă", "â", "ằ", "ầ", "ẳ", "ẩ", "ẵ", "ẫ", "ắ", "ấ", "ặ", "ậ" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "E")
+        {
+            string[] subArray = { "Ê", "Ề", "Ể", "Ễ", "Ế", "Ệ" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "e")
+        {
+            string[] subArray = { "ê", "ề", "ể", "ễ", "ế", "ệ" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "I")
+        {
+            string[] subArray = { "Ì", "Ỉ", "Ĩ", "Í", "Ị" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "i")
+        {
+            string[] subArray = { "ì", "ỉ", "ĩ", "í", "ị" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "O")
+        {
+            string[] subArray = { "Ô", "Ồ", "Ổ", "Ỗ", "Ố", "Ộ", "Ơ", "Ờ", "Ở", "Ỡ", "Ớ", "Ợ" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "o")
+        {
+            string[] subArray = { "ô", "ồ", "ổ", "ỗ", "ố", "ộ", "ơ", "ờ", "ở", "ỡ", "ớ", "ợ" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "U")
+        {
+            string[] subArray = { "Ư", "Ừ", "Ử", "Ữ", "Ứ", "Ự" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "u")
+        {
+            string[] subArray = { "ư", "ừ", "ử", "ữ", "ứ", "ự" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "Y")
+        {
+            string[] subArray = { "Ỳ", "Ỷ", "Ỹ", "Ý", "Ỵ" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "y")
+        {
+            string[] subArray = { "ỳ", "ỷ", "ỹ", "ý", "ỵ" };
+            float rand = Random.value * subArray.Length;
+            int ind = (int)rand;
+            if (ind == subArray.Length)
+            {
+                ind--;
+            }
+            return subArray[ind];
+        }
+
+        else if (c == "D")
+        {
+            return "Đ";
+        }
+
+        else if (c == "d")
+        {
+            return "đ";
+        }
+
+        return c;
+    }
+
+    string subString()
+    {
+        string ret = "";
+        for(int i = 0; i<origString.Length; i++)
+        {
+            float rand = Random.value;
+            if(rand <= subProb)
+            {
+                ret += subChar(origString.Substring(i, 1));
+            } else
+            {
+                ret += origString.Substring(i, 1);
+            }
+        }
+        return ret;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origString = text.text; // temp
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= timeToSub)
+        {
+            timer = 0;
+            text.text = subString();
+        }
+    }
+}
